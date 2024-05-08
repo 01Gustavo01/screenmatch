5f7b06c3b008face9f2b31cda541e8e2a8e61bf5
Gustavo Duarte <donhaduarte@gmail.com>
2024-05-07 23:58:49 -0300

    new case

--- _screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ _screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -22,7 +22,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
@@ -34,7 +34,8 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+
+        this.sinopse = dadosSerie.sinopse();
     }
 
     public Long getId() {
@@ -50,6 +51,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -113,12 +115,12 @@ public class Serie {
     public String toString() {
         return
                 "genero=" + genero +
-                        ", titulo='" + titulo + '\'' +
+                        ", titulo=" + titulo + '\'' +
                         ", totalTemporadas=" + totalTemporadas +
                         ", avaliacao=" + avaliacao +
-
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
-                        ", sinopse='" + sinopse + '\'';
+                        ", sinopse='" + sinopse + '\''+
+                        ", episodios='" + episodios + '\'';
     }
 }
